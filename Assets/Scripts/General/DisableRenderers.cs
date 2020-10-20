23983b473f6ed262ad50efedc622e9d120f3b087
HN-mv <HKN@makingview.com>
2020-10-20 14:17:11 +0200

    Collisions on island. Player can walk into boat

---
+++ Assets/Scripts/General/DisableRenderers.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableRenderers : MonoBehaviour
+{
+    [SerializeField] bool visibleInEditor = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!visibleInEditor || !Application.isEditor)
+        {
+            foreach (Renderer r in GetComponentsInChildren<Renderer>())
+                r.enabled = false;
+        }
+    }
+
+    private void OnValidate()
+    {
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+            r.enabled = visibleInEditor;
+    }
+}
---
+++ Assets/Scripts/Introduction/FadeIntoBoat.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeIntoBoat : MonoBehaviour
+{
+    [SerializeField] OVRPlayerController startRig = null;
+    [SerializeField] GameObject boatPosition = null;
+
+    private bool movedToBoat = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (startRig == null || boatPosition == null)
+            throw new System.Exception(gameObject.name + " null reference error");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(!movedToBoat && other.gameObject.tag.Equals("Player"))
+        {
+            StartCoroutine(GoIntoBoat());
+            movedToBoat = true;
+        }
+    }
+
+    IEnumerator GoIntoBoat()
+    {
+        float fadeTimer = 1.5f;
+        var fade = startRig.GetComponentInChildren<OVRScreenFade>();
+
+        fade.fadeTime = fadeTimer / 3;
+
+        fade.FadeIn();
+        startRig.GetComponent<CharacterController>().enabled = false;       
+        yield return new WaitForSeconds(fade.fadeTime * 2);
+
+        var camPos = startRig.GetComponentInChildren<Camera>().transform.position;
+        var offset = new Vector3 (camPos.x - startRig.transform.position.x, 0, camPos.z - startRig.transform.position.z);
+
+        startRig.transform.position = boatPosition.transform.position/* + offset*/;
+        startRig.transform.rotation = boatPosition.transform.rotation;
+        startRig.transform.parent = boatPosition.transform;
+
+        fade.FadeOut();
+        //TODO play narrator soundclip and send boat sailing
+    }
+}
